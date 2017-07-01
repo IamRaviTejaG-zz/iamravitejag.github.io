f76fd1cb1cd5cc686d3890719ecdfa23b54ad901
Ravi Teja Gannavarapu <IamRaviTejaG@users.noreply.github.com>
2017-07-01 22:25:41 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// JavaScript for index.html on iamravitejag.github.io.
+// Last Update: 01 July 2017.
+
+function showh5() {
+    document.getElementById('resume_date').style.visibility = 'visible';
+}
+
+function hideh5() {
+    document.getElementById('resume_date').style.visibility = 'hidden';
+}
