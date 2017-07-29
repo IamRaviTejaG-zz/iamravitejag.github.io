281dd0f7633e94ef393d3a7b8e3d62c46e44a317
Ravi Teja Gannavarapu <IamRaviTejaG@users.noreply.github.com>
2017-07-29 10:41:03 +0530

    Add files via upload
    Using jQuery now.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-// JavaScript for index.html on iamravitejag.github.io.
-// Last Update: 01 July 2017.
+/* JavaScript for index.html on iamravitejag.github.io */
+/* Last Update: 29 July 2017. */
 
-function showh5() {
-    document.getElementById('resume_date').style.visibility = 'visible';
-}
-
-function hideh5() {
-    document.getElementById('resume_date').style.visibility = 'hidden';
-}
+$(document).ready(function() {
+	$('#resume_date').hide();
+	$('#resume').mouseenter(function() {
+		$('#resume_date').show();
+	});
+	$('#resume').mouseleave(function() {
+		$('#resume_date').hide();
+	});
+});
