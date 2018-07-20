79ce602e80686b013abebf190dc2316fee81b3db
Ravi Teja Gannavarapu <grt4004@gmail.com>
2018-07-21 01:29:11 +0530

    Updated CSS & JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 /* JavaScript/jQuery for index.html on iamravitejag.github.io */
-/* Last Update: 17 March 2018. */
+/* Last Update: 21 July 2018. */
 
 $(document).ready(function() {
 	$('#resumedate').hide();
+	$('#gsoc-archive').hide();
+	$('#gssoc-abbr').hide();
 	$('#resume').mouseenter(function() {
 		$('#resumedate').fadeIn(200);
 	});
 	$('#resume').mouseleave(function() {
 		$('#resumedate').fadeOut(200);
+	});
+	$('#gsoc-2018-blog').mouseenter(function() {
+		$('#gsoc-archive').fadeIn(200);
+	});
+	$('#gsoc-2018-blog').mouseleave(function() {
+		$('#gsoc-archive').fadeOut(200);
+	});
+	$('#gssoc-2018-blog').mouseenter(function() {
+		$('#gssoc-abbr').fadeIn(200);
+	});
+	$('#gssoc-2018-blog').mouseleave(function() {
+		$('#gssoc-abbr').fadeOut(200);
 	});
 });
